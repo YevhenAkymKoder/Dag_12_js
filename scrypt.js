dd26b5921382b15e57b21fbc55a507096ad449f7
YevhenAkymKoder <YevhenAkym.Koder@gmail.com>
2024-03-13 11:36:34 +0100

    first commit

---
+++ scrypt.js
@@ -0,0 +1,24 @@
+function themeSwitcher() {
+    const body = document.body;
+    // Check and change body bg
+    if (body.classList.contains('light')) {
+        body.classList.remove('light');
+        body.classList.add('dark');
+    } else {
+        body.classList.remove('dark');
+        body.classList.add('light');
+    }
+}
+
+function themeBox(element) {
+    // Check and change button div bg
+    if (element.classList.contains('lightMode')) {
+        element.classList.remove('lightMode');
+        element.classList.add('darkMode');
+    } else {
+        element.classList.remove('darkMode');
+        element.classList.add('lightMode');
+    }
+}
+
+
